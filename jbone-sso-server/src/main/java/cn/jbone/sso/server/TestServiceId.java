d93421c103e0c3bc156c2c19106491a8e820b686
417511458 <417511458@qq.com>
2019-11-30 18:37:53 +0800

    授权服务serviceId

---
+++ jbone-sso-server/src/main/java/cn/jbone/sso/server/TestServiceId.java
@@ -0,0 +1,16 @@
+package cn.jbone.sso.server;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestServiceId {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("^http://sysadmin\\.local\\.jbone\\.cn");
+        Matcher matcher = pattern.matcher("http://sysadmin.local.jbone.cn");
+
+        if(matcher.find()) {
+            System.out.println(matcher.group(0));  //匹配到的整个结果
+
+        }
+    }
+}
