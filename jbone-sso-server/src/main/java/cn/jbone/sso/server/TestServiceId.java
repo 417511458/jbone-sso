d0ade4dae39ef41e9d3205540a78837f1717fab4
417511458 <417511458@qq.com>
2019-12-06 07:30:29 +0800

    sso后台管理

--- jbone-sso-server/src/main/java/cn/jbone/sso/server/TestServiceId.java
+++ jbone-sso-server/src/main/java/cn/jbone/sso/server/TestServiceId.java
@@ -7,7 +7,6 @@ public class TestServiceId {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("^http://sysadmin\\.local\\.jbone\\.cn");
         Matcher matcher = pattern.matcher("http://sysadmin.local.jbone.cn");
-
         if(matcher.find()) {
             System.out.println(matcher.group(0));  //匹配到的整个结果
 
